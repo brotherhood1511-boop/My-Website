dfdca74ed46a9cf145a9c97e24c462ba40df211f
Dominick Siciliano <brotherhood1511@gmail.com>
2026-07-06 22:04:00 -0400

    Add LinkedIn link to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 const contact = {
   email: "mailto:brotherhood1511@gmail.com",
   github: "https://github.com/brotherhood1511-boop",
+  linkedin: "https://www.linkedin.com/in/dominick-siciliano-373989230",
 };
 
 export default function Footer() {
@@ -10,6 +11,9 @@ export default function Footer() {
         <a href={contact.email}>Email</a>
         <a href={contact.github} target="_blank" rel="noreferrer">
           GitHub
+        </a>
+        <a href={contact.linkedin} target="_blank" rel="noreferrer">
+          LinkedIn
         </a>
       </div>
       <p>Dominick Siciliano</p>
